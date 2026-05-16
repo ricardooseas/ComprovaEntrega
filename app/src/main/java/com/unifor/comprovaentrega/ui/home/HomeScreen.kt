f16e0cf8c6d04ba4b8dee9dae5c22754a780b14c
GabrielSuprimio <supduuuo@gmail.com>
2026-05-16 12:49:04 -0300

    fix: remove import nao utilizado PasswordVisualTransformation do HomeScreen

--- app/src/main/java/com/unifor/comprovaentrega/ui/home/HomeScreen.kt
+++ app/src/main/java/com/unifor/comprovaentrega/ui/home/HomeScreen.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.semantics.contentDescription
